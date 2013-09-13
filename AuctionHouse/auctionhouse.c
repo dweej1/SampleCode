a5e3fb8d3a5c5a3556cf4744e664c6d938da3e40
Dwijen Patel <dwijen@gmail.com>
2013-09-13 08:31:59 -0400

    adding more descriptive error codes for API return values

--- AuctionHouse/auctionhouse.c
+++ AuctionHouse/auctionhouse.c
@@ -35,6 +35,18 @@ bool addKey(const char *auctionKey)
   return true;
 }
 
+//error codes for more descriptive return values
+typedef enum
+{
+  AUCTION_ERR_KEY = 0,
+  AUCTION_SUCCESS,
+  AUCTION_ERR_DATASTORE,
+  AUCTION_ERR_LOWBID, //not sure if this is an error or not
+  AUCTION_ERR_ENDED,
+  AUCTION_ERR_STARTED,
+  AUCTION_ERR_INPUT_NULL,
+} auctionErrorCode;
+
 //~~~~End of Auction House Data Model
 
 
@@ -42,123 +54,136 @@ bool addKey(const char *auctionKey)
 //~~~~Auction House API
 /*
 -Add an item that can be auctioned. An item has a name and reserved price
-bool addAuctionItem(const char *name, double reservePrice)
+auctionErrorCode addAuctionItem(const char *name, double reservePrice)
 
 -Start an auction on an item
-bool startAuction(const char *name)
+auctionErrorCode startAuction(const char *name)
 
 -Submit a bid to a normal auction, a bid has the bidder's number and a price, a new bid has to have a price higher than the current highest bid otherwise it's not allowed
-bool submitBidToAuction(int bidderNum, double bidderPrice, const char *auctionItemName)
+auctionErrorCode submitBidToAuction(int bidderNum, double bidderPrice, const char *auctionItemName)
 
 -Call the normal auction - bring it to an end. If the current highest bid is higher than the reserved price of the item, the auction is deemed as a success other wise it's marked as failure. The item sold should be no longer available for future auctions
-bool endAuction(const char *auctionItemName)
+auctionErrorCode endAuction(const char *auctionItemName)
 
 Query an auction - the system returns the status of a specified auction: if it's a success and if so who's the winner.
-bool queryAuction(const char *auctionItemName, int *winningBidder)
+auctionErrorCode queryAuction(const char *auctionItemName, int *winningBidder)
 */
 
 
 //Add an item that can be auctioned. An item has a name and reserve price
-bool addAuctionItem(const char *name, double reservePrice)
+auctionErrorCode addAuctionItem(const char *name, double reservePrice)
 {
   auctionData *thisAuction;
 
-  if (!addKey(name))
-    return false; //key may already exist
+  if (!addKey(name)) {
+    return AUCTION_ERR_KEY; //key may already exist, or may be "bad"
+  }
 
   thisAuction = keyLookup(name);
 
   if (thisAuction == NULL) {
-    return false; //this should be an exception because the datastore broke
+    return AUCTION_ERR_DATASTORE; //datastore invariant broken
   }
 
   thisAuction->reservePrice = reservePrice;
 
-  return true;
+  return AUCTION_SUCCESS;
 }
 
 //Start an auction on an item
-bool startAuction(const char *name)
+auctionErrorCode startAuction(const char *name)
 {
   auctionData *thisAuction = keyLookup(name);
 
   if (thisAuction == NULL) {
-    return false; //maybe have errorcodes for more granular failure
-                  //information
+    return AUCTION_ERR_KEY; 
   }
 
   if (thisAuction->auctionStarted) {
-    return false;
+    return AUCTION_ERR_STARTED;
   }
 
   if (thisAuction->auctionEnded) {
-    return false;
+    return AUCTION_ERR_ENDED;
   }
 
   thisAuction->auctionStarted = true;
 
-  return true;
+  return AUCTION_SUCCESS;
 }
 
 //Submit a bid to a normal auction, a bid has the bidder's number
 // and a price, a new bid has to have a price higher than the 
 //current highest bid otherwise it's not allowed
-bool submitBidToAuction(int bidderNum, double bidderPrice, const char *auctionItemName)
+auctionErrorCode submitBidToAuction(int bidderNum, double bidderPrice, const char *auctionItemName)
 {
   auctionData *thisAuction = keyLookup(auctionItemName);
 
   if (thisAuction == NULL) {
-    return false; 
+    return AUCTION_ERR_KEY; 
   }
 
-  if ((thisAuction->auctionStarted == false) || (thisAuction->auctionEnded == true)) {
-    return false;
+  if (thisAuction->auctionStarted == false) {
+    return AUCTION_ERR_STARTED;
+  }
+
+  if (thisAuction->auctionEnded == true) {
+    return AUCTION_ERR_ENDED;
   }
   
   if (thisAuction->highBidPrice > bidderPrice) {
-    return false;
+    return AUCTION_ERR_LOWBID;
   }
 
   thisAuction->highBidNum = bidderNum;
   thisAuction->highBidPrice = bidderPrice;
+
+  return AUCTION_SUCCESS;
 }
 
 //Call the normal auction - bring it to an end. If the current highest
 //bid is higher than the reserved price of the item, the auction is 
 //deemed as a success other wise it's marked as failure. The item sold 
 //should be no longer available for future auctions
-bool endAuction(const char *auctionItemName)
+auctionErrorCode endAuction(const char *auctionItemName)
 {
   auctionData *thisAuction = keyLookup(auctionItemName);
 
   if (thisAuction == NULL) {
-    return false; 
+    return AUCTION_ERR_KEY; 
   }
 
   if (thisAuction->auctionStarted == false) {
-    return false;
+    return AUCTION_ERR_STARTED;
   }
   
   thisAuction->auctionEnded = true;
   thisAuction->itemSold = (thisAuction->reservePrice < thisAuction->highBidPrice);
 
+  return AUCTION_SUCCESS;
 }
 
 //Query an auction - the system returns the status of a specified
 //auction: if it's a success and if so who's the winner.
-bool queryAuction(const char *auctionItemName, bool *itemSold, int *winningBidder)
+auctionErrorCode queryAuction(const char *auctionItemName, bool *itemSold, int *winningBidder)
 {
-  auctionData *thisAuction = keyLookup(auctionItemName);
+  auctionData *thisAuction;
 
-  if ((thisAuction == NULL) || (winningBidder == NULL) ||
-      (itemSold == NULL)) {
-    return false; 
+  if ((winningBidder == NULL) || (itemSold == NULL)) {
+    return AUCTION_ERR_INPUT_NULL;
   }
 
+  thisAuction = keylookup(auctionItemName);
+
+  if (thisAuction == NULL) {
+    return AUCTION_ERR_KEY;
+  }
+
+  //update return values with stored data
   *itemSold = thisAuction->itemSold;
   *winningBidder = thisAuction->highBidNum;
 
-  return true;
+  return AUCTION_SUCCESS;
 }
 
 //~~~~Auction House API
